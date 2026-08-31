49fa723666c0c444bc90bdf02b862a4a12697e4f
isabelaalmeida <isabelapantaleao24@gmail.com>
2026-08-31 08:22:56 -0300

    fix: acrescentei description e example em ViaCepResponseDTO

--- src/main/java/com/isabela/testetecnicosea/model/dto/ViaCepResponseDTO.java
+++ src/main/java/com/isabela/testetecnicosea/model/dto/ViaCepResponseDTO.java
@@ -1,11 +1,21 @@
 package com.isabela.testetecnicosea.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record ViaCepResponseDTO(
-        String cep,
+
+        @Schema(description = "Logradouro (rua/avenida)", example = "Avenida Paulista")
         String logradouro,
-        String complemento,
+
+        @Schema(description = "Bairro", example = "Bela Vista")
         String bairro,
+
+        @Schema(description = "Cidade", example = "São Paulo")
         String localidade,
+
+        @Schema(description = "UF", example = "SP")
         String uf,
+
+        @Schema(description = "Indica se o CEP consultado não foi encontrado")
         Boolean erro
 ) {}
